db9f4c6fe7729cd78fce16488e7b2e480a28910b
Xilites <3451835817@qq.com>
2023-09-08 21:25:27 +0800

    v0.0.2.0018

---
+++ src/example/content/block/CollapseMachine.java
@@ -0,0 +1,32 @@
+package example.content.block;
+
+import example.content.item.BlackHole;
+import mindustry.content.Fx;
+import mindustry.content.Items;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.world.Block;
+import mindustry.world.blocks.production.GenericCrafter;
+
+import static mindustry.content.Fx.none;
+import static mindustry.type.ItemStack.with;
+
+public class CollapseMachine {
+    public static Block CollapseMachine;
+    public static void load(){
+        CollapseMachine = new GenericCrafter("压缩机"){{
+            requirements(Category.crafting, with(Items.carbide,2));
+
+            craftEffect = none;
+            outputItem = new ItemStack(BlackHole.BlackHole,2);
+            hasItems = true;
+            hasPower = true;
+            craftTime = 60f * 2;
+            itemCapacity = 50;
+            size = 2;
+
+            consumeItem(Items.carbide,2);
+            consumePower(10f);
+        }};
+    }
+}
