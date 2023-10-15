cd478d88206d1a4b29656f55dbb1b35c07b86c88
Xilites <3451835817@qq.com>
2023-10-15 12:55:29 +0800

    v0.0.2.0073

--- src/example/content/planet.java
+++ src/example/content/planet.java
@@ -4,10 +4,15 @@ import arc.Core;
 import arc.graphics.Color;
 import arc.graphics.g2d.SpriteBatch;
 import arc.graphics.g2d.TextureRegion;
+import mindustry.content.Blocks;
+import mindustry.content.Items;
 import mindustry.content.Planets;
+import mindustry.game.Team;
 import mindustry.graphics.g3d.*;
 import mindustry.maps.planet.ErekirPlanetGenerator;
 import mindustry.type.Planet;
+import mindustry.world.meta.Attribute;
+import mindustry.world.meta.Env;
 
 public class planet {
     public static Planet
@@ -16,20 +21,43 @@ public class planet {
     NewWorld;
     public static void load(){
         OldWorldCenter = new Planet("old-world-center", Planets.sun, 4f){{
-            bloom = true;
-            accessible = true;
-            meshLoader = () -> new SunMesh(
-                    this, 5,
-                    6, 0.5, 2, 2, 1,
-                    1.1f,
-                    Color.valueOf("FF6464"),
-                    Color.valueOf("FF9696"),
-                    Color.valueOf("6464FF"),
-                    Color.valueOf("9696FF"),
-                    Color.valueOf("6496FF"),
-                    Color.valueOf("FF9664")
+            generator = new ErekirPlanetGenerator();
+            meshLoader = () -> new HexMesh(this, 5);
+            cloudMeshLoader = () -> new MultiMesh(
+                    new HexSkyMesh(this, 2, 0.15f, 0.14f, 5, Color.valueOf("eba768").a(0.75f), 2, 0.42f, 1f, 0.43f),
+                    new HexSkyMesh(this, 3, 0.6f, 0.15f, 5, Color.valueOf("eea293").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
             );
             alwaysUnlocked = true;
+            landCloudColor = Color.valueOf("ed6542");
+            atmosphereColor = Color.valueOf("f07218");
+            defaultEnv = Env.scorching | Env.terrestrial;
+            startSector = 10;
+            atmosphereRadIn = 0.02f;
+            atmosphereRadOut = 0.3f;
+            tidalLock = true;
+            orbitSpacing = 2f;
+            totalRadius += 2.6f;
+            lightSrcTo = 0.5f;
+            lightDstFrom = 0.2f;
+            clearSectorOnLose = true;
+            hiddenItems.addAll(Items.serpuloItems).removeAll(Items.erekirItems);
+
+            //TODO SHOULD there be lighting?
+            updateLighting = false;
+
+            ruleSetter = r -> {
+                r.waveTeam = Team.malis;
+                r.placeRangeCheck = false; //TODO true or false?
+                r.attributes.set(Attribute.heat, 0.8f);
+                r.showSpawns = true;
+                r.fog = true;
+                r.staticFog = true;
+                r.lighting = false;
+                r.coreDestroyClear = true;
+                r.onlyDepositCore = true; //TODO not sure
+            };
+
+            unlockedOnLand.add(Blocks.coreBastion);
         }};
         OldWorld = new Planet("old-world",OldWorldCenter,2f){{
             generator = new ErekirPlanetGenerator();
