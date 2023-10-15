eaed02064ab2fde47022a24a0a61074eec84997c
Xilites <3451835817@qq.com>
2023-10-15 13:06:40 +0800

    upload

--- src/example/content/planet.java
+++ src/example/content/planet.java
@@ -21,42 +21,12 @@ public class planet {
     NewWorld;
     public static void load(){
         OldWorldCenter = new Planet("old-world-center", Planets.sun, 4f){{
-            generator = new ErekirPlanetGenerator();
-            meshLoader = () -> new HexMesh(this, 5);
+            meshLoader = () -> new HexMesh(this, 8);
             cloudMeshLoader = () -> new MultiMesh(
-                    new HexSkyMesh(this, 2, 0.15f, 0.14f, 5, Color.valueOf("eba768").a(0.75f), 2, 0.42f, 1f, 0.43f),
-                    new HexSkyMesh(this, 3, 0.6f, 0.15f, 5, Color.valueOf("eea293").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
+                    new HexSkyMesh(this, 2, 0.2f, 0.14f, 8, Color.valueOf("eba768").a(0.75f), 2, 0.42f, 1f, 0.43f),
+                    new HexSkyMesh(this, 3, 0.6f, 0.15f, 8, Color.valueOf("eea293").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
             );
             alwaysUnlocked = true;
-            landCloudColor = Color.valueOf("ed6542");
-            atmosphereColor = Color.valueOf("f07218");
-            defaultEnv = Env.scorching | Env.terrestrial;
-            startSector = 10;
-            atmosphereRadIn = 0.02f;
-            atmosphereRadOut = 0.3f;
-            tidalLock = true;
-            orbitSpacing = 2f;
-            totalRadius += 2.6f;
-            lightSrcTo = 0.5f;
-            lightDstFrom = 0.2f;
-            clearSectorOnLose = true;
-            hiddenItems.addAll(Items.serpuloItems).removeAll(Items.erekirItems);
-
-            //TODO SHOULD there be lighting?
-            updateLighting = false;
-
-            ruleSetter = r -> {
-                r.waveTeam = Team.malis;
-                r.placeRangeCheck = false; //TODO true or false?
-                r.attributes.set(Attribute.heat, 0.8f);
-                r.showSpawns = true;
-                r.fog = true;
-                r.staticFog = true;
-                r.lighting = false;
-                r.coreDestroyClear = true;
-                r.onlyDepositCore = true; //TODO not sure
-            };
-
             unlockedOnLand.add(Blocks.coreBastion);
         }};
         OldWorld = new Planet("old-world",OldWorldCenter,2f){{
